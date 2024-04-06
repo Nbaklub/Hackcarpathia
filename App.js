ab8a7ec2fbc2ad920414394e4215187a1fc91a6c
nbaklub <mateuszsmardz@icloud.com>
2024-04-06 10:59:48 +0200

    v0.0.1

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDQQ2pUz62IpU9WoV311teVarf51ZfD7ek",
+  authDomain: "hackcarpathia.firebaseapp.com",
+  projectId: "hackcarpathia",
+  storageBucket: "hackcarpathia.appspot.com",
+  messagingSenderId: "893077155196",
+  appId: "1:893077155196:web:6b79f9f488cbdfda6148f9",
+  measurementId: "G-BB3HJ9KPMV"
+};
+const app = initializeApp(firebaseConfig);
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={} component={} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
